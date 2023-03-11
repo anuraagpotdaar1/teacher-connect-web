b5ff1b9947f9032df188b58b444bdeaaa06d8d9e
Anuraag <mailtoanuraagpotdaar@gmail.com>
2023-03-11 19:04:47 +0530

    sidenav ui changes

--- src/pages/requests.tsx
+++ src/pages/requests.tsx
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-export default function Dashboard() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+export default function Home() {
+    return (
+        <div className="m-16">
+            
+        </div>
+    )
 }
